f173534b971fb70384dec65459e515d10a6a018c
Tagada14 <jan@buszka.eu>
2021-08-27 16:04:35 +0200

    Update save.lua and scenario.lua with Ozonex map editor + remove useless markers

--- maps/FAF_TUT_OpenPalms_BO/FAF_TUT_OpenPalms_BO_scenario.lua
+++ maps/FAF_TUT_OpenPalms_BO/FAF_TUT_OpenPalms_BO_scenario.lua
@@ -1,21 +1,27 @@
-version = 3
+version = 3 -- Lua Version. Dont touch this
 ScenarioInfo = {
-    name = 'Open Palms - Build Order',
+    name = "Open Palms - Build Order",
     description = "Introduction of the map, showing important locations and initial build order.",
+    preview = '',
+    map_version = 1,
     type = 'tutorial',
     starts = true,
-    preview = '',
     size = {512, 512},
+    reclaim = {31854.73, 193523.2},
     map = '/maps/SCMP_007/SCMP_007.scmap',
     save = '/maps/FAF_TUT_OpenPalms_BO/FAF_TUT_OpenPalms_BO_save.lua',
     script = '/maps/FAF_TUT_OpenPalms_BO/FAF_TUT_OpenPalms_BO_script.lua',
-    norushradius = 0.000000,
+    norushradius = 0,
     Configurations = {
         ['standard'] = {
             teams = {
-                { name = 'FFA', armies = {'Player','Tutorial_AI','ARMY_9','NEUTRAL_CIVILIAN',} },
+                {
+                    name = 'FFA',
+                    armies = {'Player', 'Tutorial_AI', 'ARMY_9', 'NEUTRAL_CIVILIAN'}
+                },
             },
             customprops = {
             },
         },
-    }}
+    },
+}
