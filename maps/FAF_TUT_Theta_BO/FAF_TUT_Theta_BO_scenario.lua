983b6c526cd81e21fef160020d1860226bc9ac4a
Tagada14 <jan@buszka.eu>
2021-08-27 16:03:20 +0200

    Update the save.lua and scenario.lua using Ozonex map editor

--- maps/FAF_TUT_Theta_BO/FAF_TUT_Theta_BO_scenario.lua
+++ maps/FAF_TUT_Theta_BO/FAF_TUT_Theta_BO_scenario.lua
@@ -1,22 +1,27 @@
-version = 3
+version = 3 -- Lua Version. Dont touch this
 ScenarioInfo = {
-    name = 'Theta Passage - Build Order',
+    name = "Theta Passage - Build Order",
     description = "Introduction of the map, showing important locations and initial build order.",
+    preview = '',
+    map_version = 1,
     type = 'tutorial',
     starts = true,
-    preview = '',
     size = {256, 256},
+    reclaim = {6828.104, 1973.5},
     map = '/maps/SCMP_012/SCMP_012.scmap',
-    map_version = 1,
     save = '/maps/FAF_TUT_Theta_BO/FAF_TUT_Theta_BO_save.lua',
     script = '/maps/FAF_TUT_Theta_BO/FAF_TUT_Theta_BO_script.lua',
-    norushradius = 0.000000,
+    norushradius = 0,
     Configurations = {
         ['standard'] = {
             teams = {
-                { name = 'FFA', armies = {'Player','Tutorial_AI','ARMY_9','NEUTRAL_CIVILIAN',} },
+                {
+                    name = 'FFA',
+                    armies = {'Player', 'Tutorial_AI', 'ARMY_9', 'NEUTRAL_CIVILIAN'}
+                },
             },
             customprops = {
             },
         },
-    }}
+    },
+}
